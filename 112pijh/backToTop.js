8e9ae2e08a7596cda5eed7741aa6bf2913364413
yaomoking <yaomoking@hotmail.com>
2024-09-25 08:44:30 +0800

    Delete 112pijh directory

--- 112pijh/backToTop.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function() {
-    const backToTopButton = document.querySelector('.back-to-top');
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        backToTopButton.style.display = 'block';
-    } else {
-        backToTopButton.style.display = 'none';
-    }
-};
